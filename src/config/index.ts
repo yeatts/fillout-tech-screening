536155bfec553418424c72f07818ec6895aa267a
Jeremy Yeatts <jyeatts@firstorion.com>
2024-03-12 00:28:10 -0700

    changing config to error on dev .env file not being present

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 
 const envFound = dotenv.config();
-if (envFound.error) {
+if (envFound.error && process.env.NODE_ENV === 'dev') {
   // This error should crash the whole process
 
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
